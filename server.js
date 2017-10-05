26f0f8062c53bd5eaeac9af8ff2f9ba6147b7715
isai-solis <solis_isai@yahoo.com>
2017-10-04 22:30:18 -0500

    Added styling to html.

--- server.js
+++ server.js
@@ -26,3 +26,41 @@ db.on("error", function(error) {
 db.once("open", function() {
     console.log("Mongoose connection successful.");
 });
+
+app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with request
+    request("https://www.fantasypros.com/nfl/", function(error, response, html) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(html);
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("a").each(function(i, element) {
+  
+        // Save an empty result object
+        var result = {};
+  
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this).children("a").text();
+        result.link = $(this).children("a").attr("href");
+  
+        // Using our Article model, create a new entry
+        // This effectively passes the result object to the entry (and the title and link)
+        var entry = new Article(result);
+  
+        // Now, save that entry to the db
+        entry.save(function(err, doc) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+          }
+          // Or log the doc
+          else {
+            console.log(doc);
+          }
+        });
+  
+      });
+    });
+    // Tell the browser that we finished scraping the text
+    res.send("Scrape Complete");
+  });
+  
